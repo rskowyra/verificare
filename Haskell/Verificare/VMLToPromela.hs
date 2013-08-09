0fb2f090078965493954c8a5b07267d2062ccd74
lapets <lapets@gmail.com>
2013-08-09 03:16:28 -0400

    Moved some modules.

--- Haskell/Verificare/VMLToPromela.hs
+++ Haskell/Verificare/VMLToPromela.hs
@@ -1,42 +1,57 @@
+----------------------------------------------------------------
+--
+-- Verificare
+-- <description>
+--
+-- Verificare/VMLToPromela.hs
+--   Compilation of VML abstract syntax to Promela abstract
+--   syntax.
+--
 
-module VMLToPromela where
+----------------------------------------------------------------
+--
+
+module Verificare.VMLToPromela where
 
 import Data.List (nub)
 
 import qualified Language.VML.AbstractSyntax as V
 import qualified Language.Promela.AbstractSyntax as P
 
-top :: V.Top -> P.Top
-top (V.Top hs) = 
+----------------------------------------------------------------
+--
+
+root :: V.Root -> P.Root
+root (V.Root hs) = 
   let hgs = [host h | h <- hs]
       hs' = map fst hgs
       gs = concat $ map snd hgs
-  in P.Top gs hs' P.Init
+  in P.Root gs hs' P.Init
 
-host :: V.Host -> (P.Proctype, [P.GlobalDecl])
+host :: V.Host -> (P.ProcType, [P.GlobalDecl])
 host (V.Host n ds ss) =
   let ds_ss_gs_s = map decl ds
       ds' = concat [ds | (ds,ss,gs) <- ds_ss_gs_s]
       ss' = concat [ss | (ds,ss,gs) <- ds_ss_gs_s]
       gs = nub $ concat [gs | (ds,ss,gs) <- ds_ss_gs_s]
-  in (P.Proctype n [] ds' (ss' ++ map stmt ss), gs)
+  in (P.ProcType n [] ds' (ss' ++ map stmt ss), gs)
 
 decl :: V.Decl -> ([P.Decl], [P.Stmt], [P.GlobalDecl])
 decl (V.Decl t x mt) =
-  let y = 0
+  let () = ()
       
   in case t of
        V.TyInt -> 
-         ([P.Decl (ty t) x (maybe Nothing (Just . term) mt)], [], [])
+         ([P.Decl (ty t) x (maybe Nothing (Just . rhs) mt)], [], [])
 
        V.TyArray V.TyInt ->
          case mt of
            Nothing           ->
              ([P.Decl (P.TyArray 256 P.TyInt) x Nothing], [], [])
-           Just (V.Array ts) ->
+           Just (V.RHS (V.Array ts)) ->
              ( 
                [P.Decl (P.TyArray 256 P.TyInt) x Nothing], 
-               [ P.Assign (P.LHS x [P.N (toInteger i)]) (term (ts!!i)) 
+               [ P.Assign (P.LHS x [P.Index (P.N (toInteger i))]) (term (ts!!i)) 
                | i <- [0..length ts-1]
                ], 
                []
@@ -60,8 +75,8 @@ decl (V.Decl t x mt) =
            
        V.TySet V.TyInt ->
          case mt of
-           Nothing         -> ([P.Decl (P.TyChannel 256 P.TyInt) x Nothing], [], [])
-           Just (V.Set ks) -> ([P.Decl (P.TyChannel 256 P.TyInt) x Nothing], [P.COStmt (P.Send x [P.N k]) | V.N k <- ks], [])
+           Nothing                 -> ([P.Decl (P.TyChannel 256 P.TyInt) x Nothing], [], [])
+           Just (V.RHS (V.Set ks)) -> ([P.Decl (P.TyChannel 256 P.TyInt) x Nothing], [P.COpS (P.Send x [P.N k]) | V.N k <- ks], [])
 
 ty :: V.Ty -> P.Ty
 ty t = case t of
@@ -71,9 +86,9 @@ stmt :: V.Stmt -> P.Stmt
 stmt s = case s of
   V.Skip -> P.Skip
   V.Assign x t -> P.Assign (P.LHS x []) (term t)
-  V.Invoke a -> P.Skip -- ???
-  V.If f ss -> P.If [P.GuardedBlock (formula f) (map stmt ss)]
-  V.Loop ss -> P.Do [P.GuardedBlock (P.B True) (map stmt ss)]
+  V.Action a v c -> P.Skip -- ???
+  V.If f ss -> P.If $ P.GuardedBlock (formula f) (map stmt ss)
+  V.Loop ss -> P.Do $ P.GuardedBlock (P.T) (map stmt ss)
 
 formula :: V.Formula -> P.Formula
 formula f = case f of
@@ -86,14 +101,18 @@ formula f = case f of
   V.Leq t1 t2 -> P.Leq (term t1) (term t2)
   V.Gt  t1 t2 -> P.Gt (term t1) (term t2)
   V.Geq t1 t2 -> P.Geq (term t1) (term t2)
-  V.In  t1 t2 ->
+  {- V.In  t1 t2 ->
     let pt1 = term t1
         pt2 = term t2
-    in foldr P.Or (P.B False) [P.Eq pt1 (P.Index pt2 (P.N i)) | i <- [0..255]]
+    in foldr P.Or (P.F) [P.Eq pt1 (P.Index pt2 (P.N i)) | i <- [0..255]]
+  -}
+
+rhs :: V.RHS -> P.RHS
+rhs (V.RHS t) = P.RHS (term t)
 
 term :: V.Term -> P.Term
 term t = case t of
-  V.V x         -> P.V x
+  V.V x []      -> P.V x []
   V.N n         -> P.N n
   V.Neg t       -> P.Neg (term t)
   V.Plus t1 t2  -> P.Plus (term t1) (term t2)
@@ -101,6 +120,6 @@ term t = case t of
   V.Mult t1 t2  -> P.Mult (term t1) (term t2)
   V.Div t1 t2   -> P.Div (term t1) (term t2)
   V.Pow t1 t2   -> P.Pow (term t1) (term t2)
-  V.Index t1 t2 -> P.Index (term t1) (term t2)
-    
+  -- V.Index t1 t2 -> P.Index (term t1) (term t2)
+
 --eof
